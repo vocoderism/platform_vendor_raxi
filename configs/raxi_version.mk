e9f2700ec49580ea91800b90881730f730dab445
laurent <laurent.renambatz@gmail.com>
2015-11-20 13:57:53 +0100

    Bring Rafixian version (1/2)

---
+++ configs/raxi_version.mk
@@ -0,0 +1,13 @@
+#Version of raxi for all builds
+
+PRODUCT_NAME := RAXIFIAN-OS
+
+#Version details
+PRODUCT_VERSION_MAJOR = 2
+PRODUCT_VERSION_MINOR = 3
+
+RAXI_VERSION := $(PRODUCT_NAME)-$(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date + "%Y%m%d")
+
+# Write Raxifian-OS into to build.prop
+      PRODUCT_PROPERTY_OVERRIDES += \
+        ro.raxi.version=$(RAXI_VERSION)
