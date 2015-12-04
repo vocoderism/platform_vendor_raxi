dd98872d2168e6490b544e360b8ebc10a50b0665
Idrees Dargahwala <dargahidrees@gmail.com>
2015-12-04 16:07:39 +0530

    keep it aside for now

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -2,5 +2,4 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/raxi_hammerhead.mk \
     $(LOCAL_DIR)/raxi_mako.mk \
     $(LOCAL_DIR)/raxi_shamu.mk \
-    $(LOCAL_DIR)/raxi_tuna.mk \
-    $(LOCAL_DIR)/raxi_angler.mk
+    $(LOCAL_DIR)/raxi_tuna.mk 
