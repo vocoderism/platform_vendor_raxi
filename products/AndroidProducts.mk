0f984d2c32694f8e7a48ac2b9d1ec7f10980dc0b
vocoderism <davide.franzino@gmail.com>
2015-12-13 18:38:25 +0100

    Update AndroidProducts.mk
    Add n7 wifi and 3g variants to raxi support

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,5 +1,7 @@
 PRODUCT_MAKEFILES := \
+     $(LOCAL_DIR)/raxi_grouper.mk \
     $(LOCAL_DIR)/raxi_hammerhead.mk \
     $(LOCAL_DIR)/raxi_mako.mk \
     $(LOCAL_DIR)/raxi_shamu.mk \
+    $(LOCAL_DIR)/raxi_tilapia.mk \
     $(LOCAL_DIR)/raxi_tuna.mk 
